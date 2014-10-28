5d5f10559e9b0a7a075a0438d4033344a6189ba3
nkarall <nikolaus.karall@student.tuwien.ac.at>
2014-10-28 19:14:49 +0100

    Bluetooth command in Settings-Menu all working

--- Touch/gen/com/example/touch/R.java
+++ Touch/gen/com/example/touch/R.java
@@ -1091,10 +1091,10 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int bluetooth_group=0x7f05003e;
         public static final int button1=0x7f05003c;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
@@ -1106,12 +1106,15 @@ containing a value of this type.
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int get_visible=0x7f050040;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
+        public static final int listView1=0x7f05003d;
+        public static final int list_devices=0x7f050041;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
@@ -1139,6 +1142,8 @@ containing a value of this type.
         public static final int tabMode=0x7f050002;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
+        public static final int turn_off=0x7f050042;
+        public static final int turn_on=0x7f05003f;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
@@ -1240,11 +1245,12 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
-        public static final int button_value=0x7f0a0010;
-        public static final int hello_world=0x7f0a000e;
-        public static final int select_device=0x7f0a0011;
+        public static final int button_value=0x7f0a000e;
+        public static final int get_visible=0x7f0a0012;
+        public static final int list_devices=0x7f0a0011;
+        public static final int turn_off=0x7f0a0010;
+        public static final int turn_on=0x7f0a000f;
     }
     public static final class style {
         /** 
--- Touch/src/com/example/touch/MainActivity.java
+++ Touch/src/com/example/touch/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.touch;
 
 import com.example.touch.R;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -13,90 +18,93 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
-import android.bluetooth.*;
-import android.content.Intent;
+public class MainActivity extends Activity {
 
-public class MainActivity extends ActionBarActivity {
-	
-	
-	
-	private Button button;
-	private static final int REQUEST_ENABLE_BT = 2;
-	private ArrayAdapter<String> mArrayAdapter;
+	   // private Button On,Off,Visible,list;
+	   private BluetoothAdapter BA;
+	   private Set<BluetoothDevice>pairedDevices;
+	   private ListView lv;
+	   @Override
+	   protected void onCreate(Bundle savedInstanceState) {
+	      super.onCreate(savedInstanceState);
+	      setContentView(R.layout.activity_main);
+//	      On = (Button)findViewById(R.id.button1);
+//	      Off = (Button)findViewById(R.id.button2);
+//	      Visible = (Button)findViewById(R.id.button3);
+//	      list = (Button)findViewById(R.id.button4);
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-		if (mBluetoothAdapter == null) {
-		    Log.v("Bluetooth", "Device does not support bluetooth");
-		}
-		else Log.v("Bluetooth", "Yiihaaa, you have bluetooth on your device!");
-		
-		if (!mBluetoothAdapter.isEnabled()) {
-		    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-		    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-		}
-		
-		mArrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_main);
-		
-		Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-		// If there are paired devices
-		if (pairedDevices.size() > 0) {
-		    // Loop through paired devices
-		    for (BluetoothDevice device : pairedDevices) {
-		        // Add the name and address to an array adapter to show in a ListView
-		        mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
-		        Log.v("Arrays", mArrayAdapter.getItem(0));
-		    }
-		}
-		
-		
-		
-		
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		addListenerOnButton();
-	}
-	
-	public void addListenerOnButton() {
+	      lv = (ListView)findViewById(R.id.listView1);
 
-        //Select a specific button to bundle it with the action you want
-		button = (Button) findViewById(R.id.button1);
+	      BA = BluetoothAdapter.getDefaultAdapter();
+	   }
 
-		button.setOnClickListener(new OnClickListener() {
+	   public void on() {
+	      if (!BA.isEnabled()) {
+	         Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+	         startActivityForResult(turnOn, 0);
+	         Toast.makeText(getApplicationContext(),"Turned on" 
+	         ,Toast.LENGTH_LONG).show();
+	      }
+	      else{
+	         Toast.makeText(getApplicationContext(),"Already on",
+	         Toast.LENGTH_LONG).show();
+	         }
+	   }
+	   public void list() {
+	      pairedDevices = BA.getBondedDevices();
 
-			@Override
-			public void onClick(View view) {
-				Log.v("Bkl", "Button clicked");
+	      ArrayList list = new ArrayList();
+	      for(BluetoothDevice bt : pairedDevices)
+	         list.add(bt.getName());
+
+	      Toast.makeText(getApplicationContext(),"Showing Paired Devices",
+	      Toast.LENGTH_SHORT).show();
+	      final ArrayAdapter adapter = new ArrayAdapter (this,android.R.layout.simple_list_item_1, list);
+	      lv.setAdapter(adapter);
+
+	   }
+	   public void off() {
+	      BA.disable();
+	      Toast.makeText(getApplicationContext(),"Turned off" ,
+	      Toast.LENGTH_LONG).show();
+	   }
+	   public void visible() {
+	      Intent getVisible = new Intent(BluetoothAdapter.
+	      ACTION_REQUEST_DISCOVERABLE);
+	      startActivityForResult(getVisible, 0);
+
+	   }
+	   @Override
+	   public boolean onCreateOptionsMenu(Menu menu) {
+	      // Inflate the menu; this adds items to the action bar if it is present.
+	      getMenuInflater().inflate(R.menu.main, menu);
+	      return true;
+	   }
+	   
+		@Override
+		public boolean onOptionsItemSelected(MenuItem item) {
+			//Handle action bar item clicks here. The action bar will
+			//automatically handle clicks on the Home/Up button, so long
+			//as you specify a parent activity in AndroidManifest.xml.
+			int id = item.getItemId();
+			switch (id) {
+				case R.id.turn_on:
+					on();
+				break;
+				case R.id.get_visible:
+					visible();
+				break;
+				case R.id.list_devices:
+					list();
+				break;
+				case R.id.turn_off:
+					off();
+				break;
 			}
-
-		});
-
-	}
-
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+			return super.onOptionsItemSelected(item);
 		}
-		return super.onOptionsItemSelected(item);
+
 	}
-	
-	public void myfinish() {
-		finish();
-	}
-}
